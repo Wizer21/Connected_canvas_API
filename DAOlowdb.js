59151987b543678c85828f867ffccb800c0a3574
Wizer <simon.tessier8@gmail.com>
2021-01-09 19:13:58 +0100

    Creatable account

--- DAOlowdb.js
+++ DAOlowdb.js
@@ -5,12 +5,36 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({id: 0, pseudo: "new_user"}).write()
-    
-// Increment count
-db.update('id', n => n + 1).write()
+db.defaults({
+  "users": [
+    {
+      "pseudo": "defautl_user",
+      "password": "0000",
+      "id": 1,
+      "friends": []
+    }
+  ],
+      "count": 2
+  }).write()
 
 exports.getPseudo = function getPseudo(){   
     return db.get('pseudo')
             .value()        
+}
+
+exports.doPseudoExists = function doPseudoExists(newPseudo){   
+    return db.get('users')
+          .find({pseudo: newPseudo})
+          .value()
+}
+
+exports.newUser = function newUser(newPseudo, newPassword){   
+  var id = db.get('count')
+            .value()
+
+  db.get('users')
+    .push({"pseudo": newPseudo, "password": newPassword, "id": id, "friends": []})
+    .value()
+      
+  db.update('count', n => n + 1).write()
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Imports
-const http = require('http');
+const http = require('http')
 const my_lowdb = require('./DAOlowdb.js')
+const url = require('url')
 
 console.log("--- API IS STARTING ---")
 
@@ -8,16 +9,32 @@ console.log("--- API IS STARTING ---")
 var server = http.createServer(
     async function(request, answer)
     {
+        const url_parts = url.parse(request.url, true).pathname //  /create
         if(request.method === 'GET')
         {
-            if(request.url === '/pseudo' || request.url === '/pseudo/')
+            if(url_parts === '/pseudo' || url_parts === '/pseudo/')
             {
-                const data = await my_lowdb.getPseudo();
-                console.log(data)
-                answer.end(JSON.stringify(data));
+                const data = await my_lowdb.getPseudo()
+                answer.end(JSON.stringify(data))
+            }
+            else if(url_parts === '/create' || url_parts === '/create/')  // CHECK IS THE PSEUDO ALREADY EXIST
+            {   
+                const url_query = url.parse(request.url, true).query  //  url query:  [Object: null prototype] { pseudo: 'simon', pass: '1234' }
+                const checked_pseudo = url_query["pseudo"]
+                console.log("CHECK FOR :", checked_pseudo);
+
+                const userList = await my_lowdb.doPseudoExists(checked_pseudo)
+                console.log(JSON.stringify(userList))
+                if (userList == null){
+                    my_lowdb.newUser(checked_pseudo, url_query["pass"])
+                    answer.end("done")
+                }
+                else{
+                    answer.end("Nickname not avaible")                    
+                }
             }
         }
-    });
+    })
 server.listen(8080);
 
 console.log("--- API IS NOW RUNNING ---")
